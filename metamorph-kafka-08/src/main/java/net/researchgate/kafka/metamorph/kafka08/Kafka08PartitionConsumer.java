bf4d97257f988804923276e2ca075035153d2450
Janosch Woschitz <janosch.woschitz@researchgate.net>
2016-05-10 17:40:42 +0200

    added kafka-08 module

---
+++ metamorph-kafka-08/src/main/java/net/researchgate/kafka/metamorph/kafka08/Kafka08PartitionConsumer.java
@@ -0,0 +1,52 @@
+package net.researchgate.kafka.metamorph.kafka08;
+
+import net.researchgate.kafka.metamorph.PartitionConsumer;
+import net.researchgate.kafka.metamorph.PartitionConsumerRecord;
+import net.researchgate.kafka.metamorph.TopicPartition;
+import net.researchgate.kafka.metamorph.exceptions.PartitionConsumerException;
+
+import java.util.Collection;
+import java.util.List;
+
+public class Kafka08PartitionConsumer<K,V> implements PartitionConsumer<K,V> {
+
+    @Override
+    public Collection<TopicPartition> partitionsFor(String topic) throws PartitionConsumerException {
+        return null;
+    }
+
+    @Override
+    public void assign(TopicPartition partition) throws PartitionConsumerException {
+
+    }
+
+    @Override
+    public List<PartitionConsumerRecord<K, V>> poll(long timeout) throws PartitionConsumerException {
+        return null;
+    }
+
+    @Override
+    public long position() throws PartitionConsumerException {
+        return 0;
+    }
+
+    @Override
+    public long earliestPosition() throws PartitionConsumerException {
+        return 0;
+    }
+
+    @Override
+    public long latestPosition() throws PartitionConsumerException {
+        return 0;
+    }
+
+    @Override
+    public void seek(long offset) throws PartitionConsumerException {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
