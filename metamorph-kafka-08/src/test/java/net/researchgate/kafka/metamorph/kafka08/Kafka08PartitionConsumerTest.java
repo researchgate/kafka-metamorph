7fd0eb81f773017bb96158d249a0c25379694918
Janosch Woschitz <janosch.woschitz@researchgate.net>
2016-05-11 13:40:05 +0200

    extracted logic to setup embedded kafka broker for tests

--- metamorph-kafka-08/src/test/java/net/researchgate/kafka/metamorph/kafka08/Kafka08PartitionConsumerTest.java
+++ metamorph-kafka-08/src/test/java/net/researchgate/kafka/metamorph/kafka08/Kafka08PartitionConsumerTest.java
@@ -1,62 +1,42 @@
 package net.researchgate.kafka.metamorph.kafka08;
 
 import junit.framework.Assert;
-import kafka.admin.TopicCommand;
-import kafka.server.KafkaConfig;
-import kafka.server.KafkaServer;
-import kafka.utils.*;
-import kafka.zk.EmbeddedZookeeper;
 import net.researchgate.kafka.metamorph.PartitionConsumer;
 import net.researchgate.kafka.metamorph.TopicPartition;
 import net.researchgate.kafka.metamorph.exceptions.PartitionConsumerException;
-import org.I0Itec.zkclient.ZkClient;
+import net.researchgate.kafka.metamorph.kafka08.utils.KafkaTestContext;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.Collection;
-import java.util.List;
-import java.util.Properties;
-
-import static scala.collection.JavaConversions.asScalaBuffer;
 
 public class Kafka08PartitionConsumerTest {
 
-    private final static int BROKER_ID = 0;
+    private KafkaTestContext context;
+
+    @Before
+    public void setUp() {
+        context = new KafkaTestContext();
+        context.initialize();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        context.close();
+    }
 
     @Test
     public void testPartitionDiscovery() throws PartitionConsumerException {
-        EmbeddedZookeeper zkServer = new EmbeddedZookeeper(TestZKUtils.zookeeperConnect());
-        ZkClient zkClient = new ZkClient(zkServer.connectString(), 10000, 10000, ZKStringSerializer$.MODULE$);
-
-        int port = TestUtils.choosePort();
-        Properties brokerProps = TestUtils.createBrokerConfig(BROKER_ID, port, true);
-
-        KafkaConfig config = new KafkaConfig(brokerProps);
-        Time mock = new MockTime();
-
-        KafkaServer kafkaServer = new KafkaServer(config, mock);
-        kafkaServer.startup();
-
         final String topic = "test_topic";
+        context.createTopic(topic, 1);
 
-        TopicCommand.createTopic(zkClient, new TopicCommand.TopicCommandOptions(
-            new String[] {"--topic", topic, "--partitions", "1","--replication-factor", "1"}
-        ));
-
-        List<KafkaServer> servers = new ArrayList<>();
-        servers.add(kafkaServer);
-
-        TestUtils.waitUntilMetadataIsPropagated(asScalaBuffer(servers), topic, 0, 5000);
-
-        Kafka08PartitionConsumerConfig consumerConfig = new Kafka08PartitionConsumerConfig.Builder("localhost:" + port).build();
+        Kafka08PartitionConsumerConfig consumerConfig = new Kafka08PartitionConsumerConfig.Builder(context.getBootstrapServerString()).build();
         PartitionConsumer<String, String> consumer = new Kafka08PartitionConsumer<>(consumerConfig);
 
         Collection<TopicPartition> partitions = consumer.partitionsFor(topic);
         Assert.assertEquals(1, partitions.size());
         Assert.assertEquals(0, ((TopicPartition) partitions.toArray()[0]).partition());
-
-        kafkaServer.shutdown();
-        zkClient.close();
-        zkServer.shutdown();
     }
 }
---
+++ metamorph-kafka-08/src/test/java/net/researchgate/kafka/metamorph/kafka08/utils/KafkaTestContext.java
@@ -0,0 +1,91 @@
+package net.researchgate.kafka.metamorph.kafka08.utils;
+
+import kafka.admin.TopicCommand;
+import kafka.server.KafkaConfig;
+import kafka.server.KafkaServer;
+import kafka.utils.*;
+import kafka.zk.EmbeddedZookeeper;
+import org.I0Itec.zkclient.ZkClient;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static scala.collection.JavaConversions.asScalaBuffer;
+
+public class KafkaTestContext implements Closeable {
+
+    private static int sequence = 0;
+    private final int brokerId;
+
+    private EmbeddedZookeeper zkServer;
+    private ZkClient zkClient;
+    private int port;
+    private KafkaServer kafkaServer;
+    private boolean initialized;
+
+    public KafkaTestContext() {
+        brokerId = sequence++;
+    }
+
+    public void initialize() {
+        if (initialized) {
+            throw new IllegalStateException("Context has been already initialized");
+        }
+        zkServer = new EmbeddedZookeeper(TestZKUtils.zookeeperConnect());
+        zkClient = new ZkClient(zkServer.connectString(), 10000, 10000, ZKStringSerializer$.MODULE$);
+
+        port = TestUtils.choosePort();
+
+        KafkaConfig config = new KafkaConfig(TestUtils.createBrokerConfig(brokerId, port, true));
+        Time mock = new MockTime();
+
+        kafkaServer = new KafkaServer(config, mock);
+        kafkaServer.startup();
+
+        initialized = true;
+    }
+
+    public void createTopic(String topic, int numPartitions) {
+        createTopic(topic, numPartitions, 5000);
+    }
+
+    public void createTopic(String topic, int numPartitions, long timeout) {
+        ensureInitialized();
+        TopicCommand.createTopic(zkClient, new TopicCommand.TopicCommandOptions(
+                new String[] {"--topic", topic, "--partitions", String.valueOf(numPartitions),"--replication-factor", "1"}
+        ));
+
+        List<KafkaServer> servers = new ArrayList<>();
+        servers.add(kafkaServer);
+
+        for (int partitionId = 0; partitionId < numPartitions; partitionId++) {
+            TestUtils.waitUntilMetadataIsPropagated(asScalaBuffer(servers), topic, partitionId, timeout);
+        }
+    }
+
+    public String getBootstrapServerString() {
+        ensureInitialized();
+        return "localhost:" + port;
+    }
+
+    private void ensureInitialized() {
+        if (!initialized) {
+            throw new IllegalStateException("Context must be initialized, ensure that KafkaTestContext:initialize() has been invoked");
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (kafkaServer != null) {
+            kafkaServer.shutdown();
+        }
+        if (zkClient != null) {
+            zkClient.close();
+        }
+        if (zkServer != null) {
+            zkServer.shutdown();
+        }
+    }
+}
